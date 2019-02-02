082db20ddac77ee5330f740ee6ef7c34c63a1586
rattus128 <rattus128@gmail.com>
2019-02-02 19:01:30 +1100

    Sort entrances in spoiler log
    Sort entrances into to the spoiler log and drop the redundant
    child/adult qualifiers in the location/item names

--- Spoiler.py
+++ Spoiler.py
@@ -57,14 +57,13 @@ class Spoiler(object):
         for _ in range(0,5):
             self.file_hash.append(random.randint(0,31))
 
-
     def parse_data(self):
         self.locations = {}
         for world in self.worlds:
             spoiler_locations = [location for location in world.get_locations() if not location.locked and location.type != 'GossipStone']
-            sort_order = {"Song": 0, "Boss": -1}
+            sort_order = {"Song": 0, "Entrance": -1, "Boss": -2 }
             spoiler_locations.sort(key=lambda item: sort_order.get(item.type, 1))
-            self.locations[world.id] = OrderedDict([(str(location), location.item) for location in spoiler_locations])
+            self.locations[world.id] = OrderedDict([(location.spoilername(), location.item) for location in spoiler_locations])
 
 
     def to_file(self, filename):
@@ -105,7 +104,7 @@ class Spoiler(object):
         location_padding = len(max(self.locations[0].keys(), key=len)) + extra_padding
         for world in self.worlds:
             output += header_world_string.format(header="Locations", world=world.id+1)
-            output += '\n'.join(['{:{width}} {}'.format(location_string.format(location=location, world=world.id+1), item_string.format(item=item.name, player=item.world.id+1, cost=' [Costs %d Rupees]' % item.price if item.price is not None else ''), width=location_padding) for (location, item) in self.locations[world.id].items()])
+            output += '\n'.join(['{:{width}} {}'.format(location_string.format(location=location, world=world.id+1), item_string.format(item=item.spoilername(), player=item.world.id+1, cost=' [Costs %d Rupees]' % item.price if item.price is not None else ''), width=location_padding) for (location, item) in self.locations[world.id].items()])
 
         output += '\n\nPlaythrough:\n\n'
         output += '\n'.join(['%s: {\n%s\n}' % (sphere_nr, '\n'.join(['  {:{width}} {}'.format(location_string.format(location=location.name, world=location.world.id+1), item_string.format(item=item.name, player=item.world.id+1, cost=' [Costs %d Rupees]' % item.price if item.price is not None else ''), width=location_padding) for (location, item) in sphere.items()])) for (sphere_nr, sphere) in self.playthrough.items()])
